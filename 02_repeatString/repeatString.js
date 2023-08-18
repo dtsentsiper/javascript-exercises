7466e2b312ffa6451f0324ea38319287a1d7e12a
dtsentsiper <112736796+dtsentsiper@users.noreply.github.com>
2023-08-18 12:17:46 -0700

    Completed exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000)
+const repeatString = function(word, times) {
+let string = "";
+if (times < 0) return "ERROR";
+for (let i = 1; i <= times; i++) {
+string += word;  
+}
+return string;
+
 
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', number);
+repeatString('', 10);
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
